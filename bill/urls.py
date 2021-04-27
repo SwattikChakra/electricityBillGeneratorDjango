e56562ff243a58af4647f9a5c6d6775ea96d48f6
SwattikChakra <swattikroop@gmail.com>
2021-04-27 21:19:00 +0530

    Completed and ready for submission

--- bill/urls.py
+++ bill/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     # Read
     path("", views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    path("<int:pk>/update/", views.UpdateView.as_view(), name="update"),
     # Delete
     path("<int:pk>/delete/", views.DeleteView.as_view(), name="delete"),
+    # update
+    path("<int:pk>/update/", views.UpdateView.as_view(), name="update"),
 ]
